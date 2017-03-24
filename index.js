9cc52557a77e24cf0ff30da1d7d1e9ee1d812c80
ruowind <ruowind@outlook.com>
2017-03-24 18:40:48 +0800

    init

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 /**
  * Created by gengfeng on 2017/3/24.
  */
+'use strict';
+
+let through = require('through2'),
+    gutil = require('gulp-util'),
+    path = require('path');
+
+function main(config) {
+    let step1 = () => {
+
+    };
+
+    let step2 = () => {
+
+    };
+
+    return through.obj(step1, step2);
+}
+
+module.exports = main;
+
+
+
